050c1263e68b049f93476f0cabb3e45655ca047c
dmytro-poplinski <dmytro.poplinski@gmail.com>
2022-05-10 14:48:45 +0300

    added Header and Container components, realized toggle theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Header } from './components/Header'
+
 export const App = () => {
     return (
-        <div className="App">
-          Countries
-        </div>
+        <>
+            <Header />
+        </>
     )
 }
